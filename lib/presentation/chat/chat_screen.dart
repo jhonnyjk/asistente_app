1121200fde5233c748094212cd766f11396d3c2f
jhonnyjk <usuario.ikigai@email.com>
2024-06-11 16:07:06 -0500

    Creando usuarios

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:chat_bubbles/chat_bubbles.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -7,7 +8,7 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Padding(
+        leading: const Padding(
           padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
             backgroundImage: NetworkImage(
@@ -15,7 +16,44 @@ class ChatScreen extends StatelessWidget {
           ),
         ),
         centerTitle: false,
-        title: Text('Usuario'),
+        title: const Text('Usuario'),
+      ),
+      body: const _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return BubbleSpecialThree(
+                    isSender: true,
+                    text: 'Added iMessage shape bubbles',
+                    color: Color(0xFF1B97F3),
+                    tail: false,
+                    textStyle: TextStyle(color: Colors.white, fontSize: 16),
+                  );
+                },
+              ),
+            ),
+            Text('Hola'),
+          ],
+        ),
       ),
     );
   }
